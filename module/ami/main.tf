5c1601b16b28835e89127ed1c55d95ac919adcd1
Your Name <vemasani.pavani@gmail.com>
2025-07-02 13:07:29 +0530

    execute ansible packages

--- module/ami/main.tf
+++ module/ami/main.tf
@@ -7,8 +7,25 @@ resource "aws_instance" "instance" {
     }
 }
 
-# create an image from running instances
-resource "aws_ami_from_instance" "ami-instance-image" {
-  name               = "golden-ami"
-  source_instance_id = aws_instance.instance.id
+# create a provisioner to run ansible commands
+
+resource "null_resource" "provisioner" {
+    connection {
+        type         =  "ssh"
+        user         =  "ec2-user"
+        password     =  "DevOps321"
+        host         =  aws_instance.component.public_ip
+        port         =  22
+      }
+   provisioner "remote-exec" {
+    inline = [
+      "sudo pip3.11 install ansible hvac"
+      ]
+  }
 }
+
+# # create an image from running instances
+# resource "aws_ami_from_instance" "ami-instance-image" {
+#   name               = "golden-ami"
+#   source_instance_id = aws_instance.instance.id
+# }
