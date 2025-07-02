f82aa0086742d0aedf717dd0a5fbc53a767880ac
Your Name <vemasani.pavani@gmail.com>
2025-07-02 12:52:07 +0530

    added instance type

--- module/ami/main.tf
+++ module/ami/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.image_id
-  instance_type          = var.instance_type
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     Name = "${var.component}-${var.env}-ami"
