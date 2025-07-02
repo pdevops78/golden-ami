d0af957cdd59dc5acf7c59d6c659e2c8fa52d192
Your Name <vemasani.pavani@gmail.com>
2025-07-02 13:03:04 +0530

    create an image from running instance(its just like as snapshot)

--- module/ami/main.tf
+++ module/ami/main.tf
@@ -3,6 +3,12 @@ resource "aws_instance" "instance" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
-    Name = "goden-ami"
+    Name = "golden-ami"
     }
+}
+
+# create an image from running instances
+resource "aws_ami_from_instance" "ami-instance-image" {
+  name               = "golden-ami"
+  source_instance_id = aws_instance.instance.id
 }
