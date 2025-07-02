4657431f4bb2c5bb73fad68c60a42601c9570f1a
Your Name <vemasani.pavani@gmail.com>
2025-07-02 12:59:54 +0530

    no need other variables

--- module/ami/main.tf
+++ module/ami/main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "instance" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
-    Name = "${var.component}-${var.env}-ami"
+    Name = "goden-ami"
     }
 }
